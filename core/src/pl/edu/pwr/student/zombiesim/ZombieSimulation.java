8544093ab31429915145aa63473bb0a4a954f7e0
Jakub Wiczkowski <jakubwiczkowsky@gmail.com>
2023-05-14 21:49:52 +0200

    Base project structure

---
+++ core/src/pl/edu/pwr/student/zombiesim/ZombieSimulation.java
@@ -0,0 +1,37 @@
+package pl.edu.pwr.student.zombiesim;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class ZombieSimulation extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+}
